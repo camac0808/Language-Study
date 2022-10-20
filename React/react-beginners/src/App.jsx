977c04be07c70548a899738a409e563326a12b11
Hyunsik Choi <gomgom_@naver.com>
2022-10-20 17:27:19 +0900

    url 수정

--- React/react-beginners/src/App.jsx
+++ React/react-beginners/src/App.jsx
@@ -10,7 +10,7 @@ import "./style.css";
 function App() {
   return (
     <div className="App">
-      <Router basename={`${process.env.PUBLIC_URL}/`}>
+      <Router basename={`${process.env.PUBLIC_URL}`}>
         <Routes>
           <Route path={`/`} element={<Home />} />
           <Route path={`/games/:id`} element={<Detail />} />
